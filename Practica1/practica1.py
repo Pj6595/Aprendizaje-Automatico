5175c9c59b0307dd2e8274485c548b1b5da7e465
Pj <47722275+Pj6595@users.noreply.github.com>
2021-10-05 13:19:00 +0200

    Terminada la peuno

--- Practica1/practica1.py
+++ Practica1/practica1.py
@@ -5,8 +5,10 @@ from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
 
+
 def carga_csv(file_name):
-    return read_csv(file_name, header = None).to_numpy().astype(float)
+    return read_csv(file_name, header=None).to_numpy().astype(float)
+
 
 def make_data(t0_range, t1_range, X, Y):
     step = 0.1
@@ -18,6 +20,7 @@ def make_data(t0_range, t1_range, X, Y):
         Coste[ix, iy] = coste(X, Y, [Theta0[ix, iy], Theta1[ix, iy]])
     return [Theta0, Theta1, Coste]
 
+
 def coste(X, Y, Theta):
     m = len(X)
     sumatorio = 0
@@ -25,21 +28,20 @@ def coste(X, Y, Theta):
         sumatorio += ((Theta[0] + Theta[1] * X[i]) - Y[i]) ** 2
     return sumatorio / (2 * len(X))
 
+
 def dibuja_coste(Theta0, Theta1, Coste):
     fig = plt.figure()
     ax = fig.gca(projection='3d')
-
     surf = ax.plot_surface(Theta0, Theta1, Coste, cmap=cm.rainbow, linewidth=0, antialiased=False)
-
     fig.colorbar(surf, shrink=0.5, aspect=5)
-
     plt.show()
 
     fig2 = plt.figure()
-    plt.contour(Theta0, Theta1, Coste, np.logspace(-2,3,20))
+    plt.contour(Theta0, Theta1, Coste, np.logspace(-2, 3, 20))
     plt.show()
 
-def descenso_gradiente(X,Y):
+
+def descenso_gradiente(X, Y):
     m = len(X)
     alpha = 0.01
     theta_0 = theta_1 = 0
@@ -57,71 +59,108 @@ def descenso_gradiente(X,Y):
 
     Coste = coste(X, Y, (theta_0, theta_1))
 
-    #Dibujamos el resultado
+    # Dibujamos el resultado
     plt.plot(X, Y, "x")
     plt.plot([min_x, max_x], [min_y, max_y])
-    plt.savefig("descenso_gradiente.pdf")
+    plt.savefig("descenso_gradiente.png")
 
-    return (theta_0, theta_1), Coste 
+    return (theta_0, theta_1), Coste
+
 
 def apartado_1():
     data = carga_csv('ex1data1.csv')
-    X = data[:,0]
-    Y = data[:,1]
+    X = data[:, 0]
+    Y = data[:, 1]
     descenso_gradiente(X, Y)
 
-    A, B, C = make_data([-10,10], [-1,4], X, Y)
+    A, B, C = make_data([-10, 10], [-1, 4], X, Y)
     dibuja_coste(A, B, C)
 
-def normalizar(X):
-    mu = np.average(X)
-    sigma = np.std(X)
-    X_norm = X
 
-    for i in range(len(X)):
-        X_norm[i] = (X[i] - mu) / sigma
+def normalizar(X):
+    mu = X.mean(axis=0)
+    sigma = X.std(axis=0)
+    X_norm = (X - mu) / sigma
 
     return X_norm, mu, sigma
-    
 
-def apa():
-    data = carga_csv("ex1data2.csv")
-    X = data[:,0]
-    Y = data[:,1]
-    Z = data[:,2]
-    X_norm, mux, sigmax = normalizar(X)
-    Y_norm, muy, sigmay = normalizar(Y)
-    Z_norm, muz, sigmaz = normalizar(Z)
-    print(X_norm)
-    print(mux)
-    print(sigmax)
 
-def descenso_gradiente_vec(X, Y, m, Theta):
-    J = (np.transpose((X * Theta) - Y) * ((X * Theta) - Y)) / 2*m
-    return J
+def coste_vectorizado(X, Y, Theta):
+    H = np.dot(X, Theta)
+    Aux = (H - Y) ** 2
+    return Aux.sum() / (2*len(X))
+
+
+def ecuacion_normal(X, Y):
+    Theta = np.matmul(np.matmul(np.linalg.inv(np.matmul(np.transpose(X), X)), np.transpose(X)), Y)
+    return Theta
+
+
+def descenso_gradiente_vec(X, Y, alpha):
+    Theta = np.zeros(np.shape(X)[1])
+    iteraciones = 1500
+    costes = np.zeros(iteraciones)
+    for i in range(iteraciones):
+        costes[i] = coste_vectorizado(X, Y, Theta)
+        Theta = gradiente_vec(X, Y, Theta, alpha)
+
+    return Theta, costes
+
+
+def gradiente_vec(X, Y, Theta, alpha):
+    NuevaTheta = Theta
+    m = np.shape(X)[0]
+    n = np.shape(X)[1]
+    H = np.dot(X, Theta)
+    Aux = (H - Y)
+    for i in range(n):
+        Aux_i = Aux * X[:, i]
+        NuevaTheta[i] -= (alpha / m) * Aux_i.sum()
+    return NuevaTheta
+
 
 def apartado_2_1():
     data = carga_csv("ex1data2.csv")
     X = data[:, :-1]
     Y = data[:, -1]
     m = np.shape(X)[0]
-    n = np.shape(X)[1]
+
+    X, mu, sigma = normalizar(X)
     X = np.hstack([np.ones([m, 1]), X])
 
-def ecuacion_normal(X,Y):
-    Theta = np.matmul(np.matmul(np.linalg.inv(np.matmul(np.transpose(X), X)), np.transpose(X)), Y)
-    return Theta
+    alphas = [0.3, 0.1, 0.03, 0.01]
+    colors = ['indigo', 'darkviolet', 'mediumorchid', 'plum']
+
+    plt.figure()
+
+    for i in range(len(alphas)):
+        Theta, costes = descenso_gradiente_vec(X, Y, alphas[i])
+        plt.scatter(np.arange(np.shape(costes)[0]), costes, c=colors[i], label='alpha = ' + str(alphas[i]))
+        
+    plt.legend()
+    plt.savefig("pjbobo.png")
+
+    print('Theta de gradiente vectorizado: ', Theta, '\n')
+
 
 def apartado_2_2():
     data = carga_csv('ex1data2.csv')
     X = data[:, :-1]
     Y = data[:, -1]
-
     m = np.shape(X)[0]
+
+    X_norm, mu, sigma = normalizar(X)
+    X_norm = np.hstack([np.ones([m, 1]), X_norm])
+
+    theta_vec, costecitos = descenso_gradiente_vec(X_norm, Y, 0.01)
+
     X = np.hstack([np.ones([m, 1]), X])
-    theta_normal = ecuacion_normal(X,Y)
+    theta_normal = ecuacion_normal(X, Y)
 
-    print('Theta de ecuación normal: ', theta_normal, '\n')
+    pred_normal = theta_normal[0] + theta_normal[1] * 1650 + theta_normal[2] * 3
+    pred_gradient = theta_vec[0] + theta_vec[1] * ((1650 - mu[0]) / sigma[0]) + theta_vec[2] * ((3 - (mu[1]) / sigma[1]))
 
-#main()
+    print('Theta de ecuación normal: ', pred_normal, '\n')
+    print('Theta de gradiente vectorizado: ', pred_gradient, '\n')
+
 apartado_2_2()
